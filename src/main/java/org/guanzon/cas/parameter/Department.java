cd5ac83866d5ccb33540630e2cc355f746c91141
MayNevVal <valenciamaynardn@gmail.com>
2025-12-15 16:35:55 +0800

    department fix

--- src/main/java/org/guanzon/cas/parameter/Department.java
+++ src/main/java/org/guanzon/cas/parameter/Department.java
@@ -3,65 +3,70 @@ package org.guanzon.cas.parameter;
 import java.sql.SQLException;
 import org.guanzon.appdriver.agent.ShowDialogFX;
 import org.guanzon.appdriver.agent.services.Parameter;
+import org.guanzon.appdriver.base.CommonUtils;
 import org.guanzon.appdriver.base.GuanzonException;
+import org.guanzon.appdriver.constant.ClientType;
 import org.guanzon.appdriver.constant.Logical;
 import org.guanzon.appdriver.constant.UserRight;
+import org.guanzon.cas.client.ClientGUI;
+import org.guanzon.cas.client.model.Model_Client_Address;
 import org.guanzon.cas.parameter.model.Model_Department;
 import org.guanzon.cas.parameter.services.ParamModels;
 import org.json.simple.JSONObject;
 
-public class Department extends Parameter{
+public class Department extends Parameter {
+
     Model_Department poModel;
-    
+
     @Override
-    public void initialize() throws SQLException, GuanzonException{
+    public void initialize() throws SQLException, GuanzonException {
         psRecdStat = Logical.YES;
-        
+
         poModel = new ParamModels(poGRider).Department();
-        
+
         super.initialize();
     }
-    
+
     @Override
-    public JSONObject isEntryOkay() throws SQLException{
+    public JSONObject isEntryOkay() throws SQLException {
         poJSON = new JSONObject();
-        
-        if (poGRider.getUserLevel() < UserRight.SYSADMIN){
+
+        if (poGRider.getUserLevel() < UserRight.SYSADMIN) {
             poJSON.put("result", "error");
             poJSON.put("message", "User is not allowed to save record.");
             return poJSON;
         } else {
             poJSON = new JSONObject();
-            
-            if (poModel.getDescription() == null ||poModel.getDescription().isEmpty()){
+
+            if (poModel.getDescription() == null || poModel.getDescription().isEmpty()) {
                 poJSON.put("result", "error");
                 poJSON.put("message", "Description must not be empty.");
                 return poJSON;
             }
-            
-            if (poModel.getDepartmentCode()== null || poModel.getDepartmentCode().isEmpty()){
+
+            if (poModel.getDepartmentCode() == null || poModel.getDepartmentCode().isEmpty()) {
                 poJSON.put("result", "error");
                 poJSON.put("message", "Department code must not be empty.");
                 return poJSON;
             }
         }
-        
+
         poModel.setModifyingId(poGRider.Encrypt(poGRider.getUserID()));
         poModel.setModifiedDate(poGRider.getServerDate());
-        
+
         poJSON.put("result", "success");
         return poJSON;
     }
-    
+
     @Override
     public Model_Department getModel() {
         return poModel;
     }
-    
+
     @Override
-    public JSONObject searchRecord(String value, boolean byCode) throws SQLException, GuanzonException{
+    public JSONObject searchRecord(String value, boolean byCode) throws SQLException, GuanzonException {
         String lsSQL = getSQ_Browse();
-        
+
         poJSON = ShowDialogFX.Search(poGRider,
                 lsSQL,
                 value,
@@ -77,6 +82,334 @@ public class Department extends Parameter{
             poJSON.put("result", "error");
             poJSON.put("message", "No record loaded.");
             return poJSON;
+        }
+    }
+
+    public JSONObject searchDepartmentHead(String fsValue, boolean fbByCode) throws SQLException, GuanzonException, Exception {
+
+        if (fbByCode) {
+            JSONObject loJSON = new JSONObject();
+            if (fsValue.equals(getModel().DepartmentHeadAssign().getClientId())) {
+                loJSON.put("result", "success");
+                return loJSON;
+            } else {
+                loJSON.put("result", "error");
+                loJSON.put("message", "Client not found.");
+                return loJSON;
+            }
+        } else {
+            JSONObject loJSON = null;
+            if (!fsValue.isEmpty()) {
+
+                String lsSQL = "SELECT"
+                        + " sClientID"
+                        + ", sCompnyNm"
+                        + " FROM Client_Master"
+                        + " WHERE cRecdStat= '1'";
+
+                loJSON = ShowDialogFX.Search(poGRider,
+                        lsSQL,
+                        fsValue,
+                        "Client ID»Client Name",
+                        "sClientID»sCompnyNm",
+                        "sClientID»sCompnyNm",
+                        fbByCode ? 0 : 1);
+
+                if (loJSON == null) {
+                    return loJSON;
+                } else {
+                    if ("error".equals(loJSON.get("result"))) {
+                        return loJSON;
+                    }
+                }
+            }
+
+            //initialize Client GUI
+            ClientGUI loClient = new ClientGUI();
+
+            loClient.setGRider(poGRider);
+            loClient.setLogWrapper(null);
+            loClient.setCategoryCode(poGRider.getCategory());
+
+            //filter client type 
+            loClient.setClientType(ClientType.INDIVIDUAL);
+
+            //searchRecord(fsValue,fbByCode) will run make sure to set client and bycode
+            //bycode true client id
+            //bycode false company
+            //set search by code
+            loClient.setByCode(fbByCode);
+
+            if (loJSON != null) {
+                getModel().setDeptHeadAssignedId(loJSON.get("sClientID").toString());
+
+                //set client id
+                loClient.setClientId(getModel().getDeptHeadAssignedId());
+
+            } else {
+                loClient.setClientId("");
+            }
+
+            //load record
+            CommonUtils.showModal(loClient);
+
+            //initialize new json for result
+            JSONObject loResult = new JSONObject();
+
+            //load if button 
+            if (!loClient.isCancelled()) {
+
+                getModel().setDeptHeadAssignedId(loClient.getClient().getModel().getClientId() != null ? loClient.getClient().getModel().getClientId() : "");
+
+            }
+            loResult.put("result", "success");
+            return loResult;
+        }
+    }
+
+    public JSONObject searchHead(String fsValue, boolean fbByCode) throws SQLException, GuanzonException, Exception {
+
+        if (fbByCode) {
+            JSONObject loJSON = new JSONObject();
+            if (fsValue.equals(getModel().HeadAssign().getClientId())) {
+                loJSON.put("result", "success");
+                return loJSON;
+            } else {
+                loJSON.put("result", "error");
+                loJSON.put("message", "Client not found.");
+                return loJSON;
+            }
+        } else {
+            JSONObject loJSON = null;
+            if (!fsValue.isEmpty()) {
+
+                String lsSQL = "SELECT"
+                        + " sClientID"
+                        + ", sCompnyNm"
+                        + " FROM Client_Master"
+                        + " WHERE cRecdStat= '1'";
+
+                loJSON = ShowDialogFX.Search(poGRider,
+                        lsSQL,
+                        fsValue,
+                        "Client ID»Client Name",
+                        "sClientID»sCompnyNm",
+                        "sClientID»sCompnyNm",
+                        fbByCode ? 0 : 1);
+
+                if (loJSON == null) {
+                    return loJSON;
+                } else {
+                    if ("error".equals(loJSON.get("result"))) {
+                        return loJSON;
+                    }
+                }
+            }
+
+            //initialize Client GUI
+            ClientGUI loClient = new ClientGUI();
+
+            loClient.setGRider(poGRider);
+            loClient.setLogWrapper(null);
+            loClient.setCategoryCode(poGRider.getCategory());
+
+            //filter client type 
+            loClient.setClientType(ClientType.INDIVIDUAL);
+
+            //searchRecord(fsValue,fbByCode) will run make sure to set client and bycode
+            //bycode true client id
+            //bycode false company
+            //set search by code
+            loClient.setByCode(fbByCode);
+
+            if (loJSON != null) {
+                getModel().setDeptHeadId(loJSON.get("sClientID").toString());
+
+                //set client id
+                loClient.setClientId(getModel().getDeptHeadId());
+
+            } else {
+                loClient.setClientId("");
+            }
+
+            //load record
+            CommonUtils.showModal(loClient);
+
+            //initialize new json for result
+            JSONObject loResult = new JSONObject();
+
+            //load if button 
+            if (!loClient.isCancelled()) {
+
+                getModel().setDeptHeadId(loClient.getClient().getModel().getClientId() != null ? loClient.getClient().getModel().getClientId() : "");
+
+            }
+            loResult.put("result", "success");
+            return loResult;
+        }
+    }
+
+    public JSONObject searchSupervisor(String fsValue, boolean fbByCode) throws SQLException, GuanzonException, Exception {
+
+        if (fbByCode) {
+            JSONObject loJSON = new JSONObject();
+            if (fsValue.equals(getModel().SupervisorAssign().getClientId())) {
+                loJSON.put("result", "success");
+                return loJSON;
+            } else {
+                loJSON.put("result", "error");
+                loJSON.put("message", "Client not found.");
+                return loJSON;
+            }
+        } else {
+            JSONObject loJSON = null;
+            if (!fsValue.isEmpty()) {
+
+                String lsSQL = "SELECT"
+                        + " sClientID"
+                        + ", sCompnyNm"
+                        + " FROM Client_Master"
+                        + " WHERE cRecdStat= '1'";
+
+                loJSON = ShowDialogFX.Search(poGRider,
+                        lsSQL,
+                        fsValue,
+                        "Client ID»Client Name",
+                        "sClientID»sCompnyNm",
+                        "sClientID»sCompnyNm",
+                        fbByCode ? 0 : 1);
+
+                if (loJSON == null) {
+                    return loJSON;
+                } else {
+                    if ("error".equals(loJSON.get("result"))) {
+                        return loJSON;
+                    }
+                }
+            }
+
+            //initialize Client GUI
+            ClientGUI loClient = new ClientGUI();
+
+            loClient.setGRider(poGRider);
+            loClient.setLogWrapper(null);
+            loClient.setCategoryCode(poGRider.getCategory());
+
+            //filter client type 
+            loClient.setClientType(ClientType.INDIVIDUAL);
+
+            //searchRecord(fsValue,fbByCode) will run make sure to set client and bycode
+            //bycode true client id
+            //bycode false company
+            //set search by code
+            loClient.setByCode(fbByCode);
+
+            if (loJSON != null) {
+                getModel().setDeptSupervisorAssignedId(loJSON.get("sClientID").toString());
+
+                //set client id
+                loClient.setClientId(getModel().getDeptSupervisorAssignedId());
+
+            } else {
+                loClient.setClientId("");
+            }
+
+            //load record
+            CommonUtils.showModal(loClient);
+
+            //initialize new json for result
+            JSONObject loResult = new JSONObject();
+
+            //load if button 
+            if (!loClient.isCancelled()) {
+
+                getModel().setDeptSupervisorAssignedId(loClient.getClient().getModel().getClientId() != null ? loClient.getClient().getModel().getClientId() : "");
+
+            }
+            loResult.put("result", "success");
+            return loResult;
+        }
+    }
+
+    public JSONObject searchGeneral(String fsValue, boolean fbByCode) throws SQLException, GuanzonException, Exception {
+
+        if (fbByCode) {
+            JSONObject loJSON = new JSONObject();
+            if (fsValue.equals(getModel().GeneralAssign().getClientId())) {
+                loJSON.put("result", "success");
+                return loJSON;
+            } else {
+                loJSON.put("result", "error");
+                loJSON.put("message", "Client not found.");
+                return loJSON;
+            }
+        } else {
+            JSONObject loJSON = null;
+            if (!fsValue.isEmpty()) {
+
+                String lsSQL = "SELECT"
+                        + " sClientID"
+                        + ", sCompnyNm"
+                        + " FROM Client_Master"
+                        + " WHERE cRecdStat= '1'";
+
+                loJSON = ShowDialogFX.Search(poGRider,
+                        lsSQL,
+                        fsValue,
+                        "Client ID»Client Name",
+                        "sClientID»sCompnyNm",
+                        "sClientID»sCompnyNm",
+                        fbByCode ? 0 : 1);
+
+                if (loJSON == null) {
+                    return loJSON;
+                } else {
+                    if ("error".equals(loJSON.get("result"))) {
+                        return loJSON;
+                    }
+                }
+            }
+
+            //initialize Client GUI
+            ClientGUI loClient = new ClientGUI();
+
+            loClient.setGRider(poGRider);
+            loClient.setLogWrapper(null);
+            loClient.setCategoryCode(poGRider.getCategory());
+
+            //filter client type 
+            loClient.setClientType(ClientType.INDIVIDUAL);
+
+            //searchRecord(fsValue,fbByCode) will run make sure to set client and bycode
+            //bycode true client id
+            //bycode false company
+            //set search by code
+            loClient.setByCode(fbByCode);
+
+            if (loJSON != null) {
+                getModel().setGeneralAssignedId(loJSON.get("sClientID").toString());
+
+                //set client id
+                loClient.setClientId(getModel().getGeneralAssignedId());
+
+            } else {
+                loClient.setClientId("");
+            }
+
+            //load record
+            CommonUtils.showModal(loClient);
+
+            //initialize new json for result
+            JSONObject loResult = new JSONObject();
+
+            //load if button 
+            if (!loClient.isCancelled()) {
+
+                getModel().setGeneralAssignedId(loClient.getClient().getModel().getClientId() != null ? loClient.getClient().getModel().getClientId() : "");
+
+            }
+            loResult.put("result", "success");
+            return loResult;
         }
     }
 }
--- src/main/java/org/guanzon/cas/parameter/model/Model_Department.java
+++ src/main/java/org/guanzon/cas/parameter/model/Model_Department.java
@@ -7,44 +7,64 @@ import org.guanzon.appdriver.base.GuanzonException;
 import org.guanzon.appdriver.base.MiscUtil;
 import org.guanzon.appdriver.constant.EditMode;
 import org.guanzon.appdriver.constant.RecordStatus;
+import org.guanzon.cas.client.model.Model_Client_Master;
+import org.guanzon.cas.client.services.ClientModels;
 import org.guanzon.cas.parameter.services.ParamModels;
 import org.json.simple.JSONObject;
 
 public class Model_Department extends Model {
+
     private Model_Industry poIndustry;
-    
+    private Model_Client_Master poDeptHead;
+    private Model_Client_Master poHead;
+    private Model_Client_Master poSupervisor;
+    private Model_Client_Master poGeneral;
+    private Model_Branch poMain;
+
     @Override
     public void initialize() {
         try {
             poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
-            
+
             poEntity.last();
             poEntity.moveToInsertRow();
 
             MiscUtil.initRowSet(poEntity);
-            
-            //assign default values
-            poEntity.updateString("cRecdStat", RecordStatus.ACTIVE);
-            //end - assign default values
 
             poEntity.insertRow();
             poEntity.moveToCurrentRow();
 
             poEntity.absolute(1);
 
+            //assign default values
+            poEntity.updateString("cRecdStat", RecordStatus.ACTIVE);
+            poEntity.updateNull("sDeptHead");
+            poEntity.updateNull("sMobileNo");
+            poEntity.updateNull("sEMailAdd");
+            poEntity.updateNull("sMainIDxx");
+            poEntity.updateNull("sHAssgnID");
+            poEntity.updateNull("sSAssgnID");
+            poEntity.updateNull("sGenMgrID");
+            poEntity.updateNull("cEntLevel");
+            //end - assign default values
+
             ID = poEntity.getMetaData().getColumnLabel(1);
-            
+
             //initialize other connections
             poIndustry = new ParamModels(poGRider).Industry();
+            poDeptHead = new ClientModels(poGRider).ClientMaster();
+            poHead = new ClientModels(poGRider).ClientMaster();
+            poSupervisor = new ClientModels(poGRider).ClientMaster();
+            poGeneral = new ClientModels(poGRider).ClientMaster();
             //end - initialize other connections
-            
+
             pnEditMode = EditMode.UNKNOWN;
         } catch (SQLException e) {
             logwrapr.severe(e.getMessage());
             System.exit(1);
         }
     }
-    
+
     public JSONObject setDepartmentId(String departmentId) {
         return setValue("sDeptIDxx", departmentId);
     }
@@ -68,7 +88,7 @@ public class Model_Department extends Model {
     public String getDeptHeadId() {
         return (String) getValue("sDeptHead");
     }
-    
+
     public JSONObject setDeptMobileNo(String mobileNo) {
         return setValue("sMobileNo", mobileNo);
     }
@@ -76,7 +96,7 @@ public class Model_Department extends Model {
     public String getDeptMobileNo() {
         return (String) getValue("sMobileNo");
     }
-    
+
     public JSONObject setDeptEmail(String emailAddress) {
         return setValue("sEMailAdd", emailAddress);
     }
@@ -84,7 +104,15 @@ public class Model_Department extends Model {
     public String getDeptEmail() {
         return (String) getValue("sEMailAdd");
     }
-    
+
+    public JSONObject setMainID(String mainid) {
+        return setValue("sMainIDxx", mainid);
+    }
+
+    public String getMainID() {
+        return (String) getValue("sMainIDxx");
+    }
+
     public JSONObject setDepartmentCode(String departmentCode) {
         return setValue("sDeptCode", departmentCode);
     }
@@ -92,7 +120,7 @@ public class Model_Department extends Model {
     public String getDepartmentCode() {
         return (String) getValue("sDeptCode");
     }
-    
+
     public JSONObject setDeptHeadAssignedId(String employeeId) {
         return setValue("sHAssgnID", employeeId);
     }
@@ -100,7 +128,7 @@ public class Model_Department extends Model {
     public String getDeptHeadAssignedId() {
         return (String) getValue("sHAssgnID");
     }
-    
+
     public JSONObject setDeptSupervisorAssignedId(String employeeId) {
         return setValue("sSAssgnID", employeeId);
     }
@@ -109,41 +137,57 @@ public class Model_Department extends Model {
         return (String) getValue("sSAssgnID");
     }
 
+    public JSONObject setGeneralAssignedId(String generalassignid) {
+        return setValue("sGenMgrID", generalassignid);
+    }
+
+    public String getGeneralAssignedId() {
+        return (String) getValue("sGenMgrID");
+    }
+
+    public JSONObject setEntLevel(String entlevel) {
+        return setValue("cEntLevel", entlevel);
+    }
+
+    public String getEntLevel() {
+        return (String) getValue("cEntLevel");
+    }
+
     public JSONObject setRecordStatus(String recordStatus) {
         return setValue("cRecdStat", recordStatus);
     }
 
     public String getRecordStatus() {
         return (String) getValue("cRecdStat");
-    } 
-    
-    public JSONObject setModifyingId(String modifyingId){
+    }
+
+    public JSONObject setModifyingId(String modifyingId) {
         return setValue("sModified", modifyingId);
     }
-    
-    public String getModifyingId(){
+
+    public String getModifyingId() {
         return (String) getValue("sModified");
     }
-    
-    public JSONObject setModifiedDate(Date modifiedDate){
+
+    public JSONObject setModifiedDate(Date modifiedDate) {
         return setValue("dModified", modifiedDate);
     }
-    
-    public Date getModifiedDate(){
+
+    public Date getModifiedDate() {
         return (Date) getValue("dModified");
     }
-    
-    public Model_Industry Industry() throws SQLException, GuanzonException{
-        if (!"".equals((String) getValue("sIndstCdx"))){
-            if (poIndustry.getEditMode() == EditMode.READY && 
-                poIndustry.getIndustryId().equals((String) getValue("sIndstCdx")))
+
+    public Model_Industry Industry() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sIndstCdx"))) {
+            if (poIndustry.getEditMode() == EditMode.READY
+                    && poIndustry.getIndustryId().equals((String) getValue("sIndstCdx"))) {
                 return poIndustry;
-            else{
+            } else {
                 poJSON = poIndustry.openRecord((String) getValue("sIndstCdx"));
 
-                if ("success".equals((String) poJSON.get("result")))
+                if ("success".equals((String) poJSON.get("result"))) {
                     return poIndustry;
-                else {
+                } else {
                     poIndustry.initialize();
                     return poIndustry;
                 }
@@ -153,9 +197,114 @@ public class Model_Department extends Model {
             return poIndustry;
         }
     }
-    
+
+    public Model_Client_Master DepartmentHeadAssign() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sDeptHead"))) {
+            if (poDeptHead.getEditMode() == EditMode.READY
+                    && poDeptHead.getClientId().equals((String) getValue("sDeptHead"))) {
+                return poDeptHead;
+            } else {
+                poJSON = poDeptHead.openRecord((String) getValue("sDeptHead"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poDeptHead;
+                } else {
+                    poDeptHead.initialize();
+                    return poDeptHead;
+                }
+            }
+        } else {
+            poDeptHead.initialize();
+            return poDeptHead;
+        }
+    }
+
+    public Model_Client_Master HeadAssign() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sHAssgnID"))) {
+            if (poHead.getEditMode() == EditMode.READY
+                    && poHead.getClientId().equals((String) getValue("sHAssgnID"))) {
+                return poHead;
+            } else {
+                poJSON = poHead.openRecord((String) getValue("sHAssgnID"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poHead;
+                } else {
+                    poHead.initialize();
+                    return poHead;
+                }
+            }
+        } else {
+            poHead.initialize();
+            return poHead;
+        }
+    }
+
+    public Model_Client_Master SupervisorAssign() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sSAssgnID"))) {
+            if (poSupervisor.getEditMode() == EditMode.READY
+                    && poSupervisor.getClientId().equals((String) getValue("sSAssgnID"))) {
+                return poSupervisor;
+            } else {
+                poJSON = poSupervisor.openRecord((String) getValue("sSAssgnID"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poSupervisor;
+                } else {
+                    poSupervisor.initialize();
+                    return poSupervisor;
+                }
+            }
+        } else {
+            poSupervisor.initialize();
+            return poSupervisor;
+        }
+    }
+
+    public Model_Client_Master GeneralAssign() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sGenMgrID"))) {
+            if (poGeneral.getEditMode() == EditMode.READY
+                    && poGeneral.getClientId().equals((String) getValue("sGenMgrID"))) {
+                return poGeneral;
+            } else {
+                poJSON = poGeneral.openRecord((String) getValue("sGenMgrID"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poGeneral;
+                } else {
+                    poGeneral.initialize();
+                    return poGeneral;
+                }
+            }
+        } else {
+            poGeneral.initialize();
+            return poGeneral;
+        }
+    }
+
+    public Model_Branch MainBranch() throws SQLException, GuanzonException {
+        if (!"".equals((String) getValue("sMainIDxx"))) {
+            if (poMain.getEditMode() == EditMode.READY
+                    && poMain.getBranchCode().equals((String) getValue("sMainIDxx"))) {
+                return poMain;
+            } else {
+                poJSON = poMain.openRecord((String) getValue("sMainIDxx"));
+
+                if ("success".equals((String) poJSON.get("result"))) {
+                    return poMain;
+                } else {
+                    poMain.initialize();
+                    return poMain;
+                }
+            }
+        } else {
+            poMain.initialize();
+            return poMain;
+        }
+    }
+
     @Override
     public String getNextCode() {
-        return MiscUtil.getNextCode(getTable(), ID, false, poGRider.getGConnection().getConnection(), "");
+        return MiscUtil.getNextCode(getTable(), ID, false, poGRider.getGConnection().getConnection(), poGRider.getBranchCode().substring(0, 1));
     }
 }
